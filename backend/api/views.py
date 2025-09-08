f6299b5fc0f20719bfe6238fa72e793f94d5bacf
TFlanDev <Tdflan90@gmail.com>
2025-09-07 17:26:10 -0700

    feat: Add user registration and authentication endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import userSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = userSerializer
+    permission_classes = [AllowAny]
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+
+# Todo Web App (In Development)
+
+Early stages of a  to-do web application. The project is built using the Django framework and will feature a React frontend.
